96109f1c2efd346d123fe548ed5fe51690b69e10
Ldpavlov <simeonpavlov07@gmail.com>
2022-07-19 11:49:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dq9bdu-4fog41
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dq9bdu-4fog41)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+
+  constructor() {}
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  getItems() {
+    return this.items;
+  }
+}
